fde700695e874ef9551a41828ca3965e2e86bdc1
thesacredcoder <akashsamal4875@gmail.com>
2021-03-07 23:51:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Please push hojana.</p>
+        <p>Please push hojana!!!!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
